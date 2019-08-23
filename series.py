c07b8f3f13ca1c71e41a7973cf633af7ce32d27e
Ovilord1992 <53706170+Ovilord1992@users.noreply.github.com>
2019-08-23 03:04:35 +0300

    Update series.py

--- series.py
+++ series.py
@@ -3,10 +3,10 @@ import time
 N = int(input('N: '))
 start_time = time.time()
 def p(N):
-  s = set(range(2, N))
+  s = set(range(1, N, 2))
   for i in range(2, int(math.sqrt(N))):
     if i in s:
-      s -= set(range(2*i, N, i))
+      s -= set(range(i*i, N, i))
   f = len(s)
   print(f'Found: {f}')
   print(f'Percent: {100/N*f}')
